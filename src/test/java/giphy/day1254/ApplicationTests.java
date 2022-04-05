5b2e8449fac4ab1bf27aa091d452a7ecce88ded3
siawli <tiosiawlilili@gmail.com>
2022-04-05 11:05:06 +0800

    first commit

---
+++ src/test/java/giphy/day1254/ApplicationTests.java
@@ -0,0 +1,13 @@
+package giphy.day1254;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
